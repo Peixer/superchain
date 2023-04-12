e4f605aec769bc529a172051daab3661ef385ffe
Glaicon <gjpeixer@gmail.com>
2023-04-12 13:54:10 -0300

    update .env

--- vue/src/env.ts
+++ vue/src/env.ts
@@ -1,5 +1,5 @@
-const apiURL = import.meta.env.VITE_API_COSMOS ?? "http://localhost:1317";
-const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "http://localhost:26657";
+const apiURL = import.meta.env.VITE_API_COSMOS ?? "http://164.90.168.110:1317";
+const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "http://164.90.168.110:26657";
 const prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? "cosmos";
 
 export const env = {
